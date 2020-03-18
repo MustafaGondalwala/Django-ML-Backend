d8160e7c1ef4163d86d403d7e54d350180072df8
mustafa <mustafagondalwala32@gmail.com>
2020-03-18 17:19:06 +0530

    updated cloudinary url's

--- cloudinary/urls.py
+++ cloudinary/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('api/user/register',views.register),
-    path('api/user/login',views.login),
-    path('api/file/sendImageDetails',views.sendImageDetails),
-    path('api/file',views.file),
-    path('api/file/deleteImage',views.deleteImage),
-    path('api/file/search',views.search)
+    path('user/register',views.register),
+    path('user/login',views.login),
+    path('file/sendImageDetails',views.sendImageDetails),
+    path('file',views.file),
+    path('file/deleteImage',views.deleteImage),
+    path('file/search',views.search)
 
 ]
