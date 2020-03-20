a7d1708c1e31cfc262642c920499694b60b1558a
mustafa <mustafagondalwala32@gmail.com>
2020-03-20 15:01:23 +0530

    removed cloudinary

--- cloudinary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CloudinaryConfig(AppConfig):
-    name = 'cloudinary'
--- django_backend/urls.py
+++ django_backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/face-identification/',include('face_identification.urls')),
-    path('api/cloudinary/',include('cloudinary.urls')),
+#    path('api/cloudinary/',include('cloudinary.urls')),
     path('api/scrapper/',include('scraper.urls')),
     path('api/news-summary/',include('news_summary.urls')),
     path('api/timetracker/',include('timetracker.urls'))
